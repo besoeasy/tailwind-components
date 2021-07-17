72752790cbe5d572def8cdf83fa6a3a7b994dd4f
BeSoEasy <besoeasy@users.noreply.github.com>
2021-07-17 07:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-var glob = require('glob');
 var fs = require('fs');
-var copydir = require('copy-dir');
+var glob = require('glob');
 
-// content to inject
-content = `
+var content = `
 
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <script defer src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js"></script>
 
 `;
 
-//logic
-fs.rmdir('dist', { recursive: true }, (err) => {
-	if (err) {
-		throw err;
+function deletedirectory(path) {
+	if (fs.existsSync(path)) {
+		fs.readdirSync(path).forEach(function (file, index) {
+			var curPath = path + '/' + file;
+			if (fs.statSync(curPath).isDirectory()) {
+				deletedirectory(curPath);
+			} else {
+				fs.unlinkSync(curPath);
+			}
+		});
+		fs.rmdirSync(path);
 	}
-	console.log('Removed Cache');
+}
 
-	copydir(
-		'components',
-		'dist',
-		{
-			utimes: true, // keep add time and modify time
-			mode: true, // keep file mode
-			cover: true, // cover file when exists, default is true
-		},
-		function (err) {
-			if (err) throw err;
+function copydirectory(src, dest) {
+	fs.mkdirSync(dest);
+	fs.readdirSync(src).forEach(function (file) {
+		var curPath = src + '/' + file;
+		if (fs.statSync(curPath).isDirectory()) {
+			copydirectory(curPath, dest + '/' + file);
+		} else {
+			fs.copyFileSync(curPath, dest + '/' + file);
+		}
+	});
+}
 
-			console.log('Build New Cache');
+async function main() {
+	await deletedirectory('./dist/');
+	await copydirectory('./components/', './dist/');
 
-			glob('dist/**/*.html', function (er, files) {
-				console.log('Total Components : ' + files.length);
+	await glob('dist/**/*.html', function (er, files) {
+		console.log('Total Components : ' + files.length);
 
-				files.forEach((filename) => {
-					try {
-						fs.appendFile(filename, content, (err) => {
-							if (err) {
-								console.error(err);
-							}
-						});
-					} catch (err) {
+		files.forEach((filename) => {
+			try {
+				fs.appendFile(filename, content, (err) => {
+					if (err) {
 						console.error(err);
 					}
 				});
+			} catch (err) {
+				console.error(err);
+			}
+		});
 
-				console.log('Tags Injected');
-			});
-		}
-	);
-});
+		console.log('Tags Injected');
+	});
+}
+
+main();
