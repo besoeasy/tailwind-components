0a409047887cd5d33643c198afed3383b04f6638
lordlokion <akhilsharma870@gmail.com>
2023-12-16 20:28:34 +0530

    ..

--- make.js
+++ make.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-const extraname = "uinel-cta-"
+const extraname = "uinel-contacts-"
 
 function generateRandomFileName() {
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
