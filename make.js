9db58f4c86950589e8cf28a81245601046f19448
besoeasy <besoeasy@users.noreply.github.com>
2023-12-16 16:33:45 +0530

    Add "make" script to package.json

---
+++ make.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const extraname = "coleos-"
+
+function generateRandomFileName() {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomName = '';
+  for (let i = 0; i < 8; i++) {
+    randomName += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return `components/unsorted/${extraname}${randomName}.html`; // Updated path
+}
+
+function createHTMLFile() {
+  const fileName = generateRandomFileName();
+  const htmlContent = '<div> </div>';
+
+  fs.writeFile(fileName, htmlContent, (err) => {
+    if (err) {
+      console.error('Error creating HTML file:', err);
+    } else {
+      console.log(`Created HTML file: ${fileName}`);
+      openInVSCode(fileName);
+    }
+  });
+}
+
+function openInVSCode(fileName) {
+  exec(`code ${fileName}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error('Error opening file in VS Code:', err);
+    } else {
+      console.log(`Opened ${fileName} in VS Code`);
+    }
+  });
+}
+
+createHTMLFile();
