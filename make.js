20010e51d05e4532315d0d0568a708547b07ce16
a4akki <akhilsharma870@gmail.com>
2023-12-17 06:45:15 -0800

    ..

--- make.js
+++ make.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-const extraname = "bendis-all-"
+const extraname = "suncealand-all-"
 
 function generateRandomFileName() {
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
