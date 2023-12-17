3781f8ef0ad627be7b9c9798ee70b431e39de4a2
lordlokion <akhilsharma870@gmail.com>
2023-12-17 12:12:41 +0530

    ..

--- make.js
+++ make.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-const extraname = "uinel-cookies-"
+const extraname = "nigod-footer-"
 
 function generateRandomFileName() {
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
